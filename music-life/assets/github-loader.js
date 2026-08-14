fc24cce938f46eb1bc34e4c5ca14f10660d5c337
backfire0190-coder <backfire0190@gmail.com>
2026-08-14 11:02:08 +0800

    Serve music-life bundle in browser-safe chunks

--- music-life/assets/github-loader.js
+++ music-life/assets/github-loader.js
@@ -1,4 +1,4 @@
-const partUrls = Array.from({ length: 5 }, (_, index) => `./index-BYu0IQkG.part${index}.bin`);
+const partUrls = Array.from({ length: 12 }, (_, index) => `./index-BYu0IQkG.part${index}.txt`);
 
 try {
   const buffers = await Promise.all(partUrls.map(async (url) => {
